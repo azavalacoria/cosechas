867f908ed15d5a2c9edd896ead27bf2596d45080
Abelardo Zavala <azavalacoria@icloud.com>
2022-01-09 08:07:03 -0600

    Initial Commit

---
+++ Cosechas/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cosechas
+//
+//  Created by Adrian Zavala Coria on 09/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
